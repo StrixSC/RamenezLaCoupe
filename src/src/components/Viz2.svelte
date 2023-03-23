59fa580e0a3a9abbc129a2c60b6853e11e1f7bdd
StrixSC <talon.strix@gmail.com>
2023-03-23 19:02:31 -0400

    svelte project

---
+++ src/src/components/Viz2.svelte
@@ -0,0 +1,1 @@
+<h1>Ceci est la viz1</h1>
---
+++ src/src/routes/viz1/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import Viz1 from "../../components/Viz1.svelte";
+    import * as d3 from "d3";
+    console.log(d3);
+</script>
+
+<main>
+    <Viz1></Viz1>
+    
+</main>
