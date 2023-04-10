33cf74ece41dff560c90d708e493e9295307ad7b
StrixSC <talon.strix@gmail.com>
2023-04-09 20:51:27 -0400

    viz3 wip

--- src/components/Viz3.svelte
+++ src/components/Viz3.svelte
@@ -1,133 +1,204 @@
 <script lang="ts">
     import * as d3 from "d3";
 
-    import type { Dimensions } from "../models/dimensions";
     import { onMount } from "svelte";
-    import { findMaximumKeyValue, getHighestColumnKey, parseData } from "../utils/viz3-helpers";
+    import {
+        getLargestValue,
+        getDataGroups,
+        getDataSubgroups,
+        transformData,
+    } from "../utils/viz3-helpers";
+    import type { PlayerData } from "src/models/france-player-data";
 
-    export let datafile: string;
-
-    const margins = { top: 80, right: 0, bottom: 80, left: 55 } as Dimensions;
-    let bounds;
-    let svgSize;
-    let graphSize;
-    const colors = [
-        '#ad8c2f',
-        '#97a515',
-        '#8a766b',
-        '#781d20',
-        '#9b7f68',
-        '#b0714c',
-        '#a13d79',
-    ]
+    let _data: PlayerData[][] = [];
+    let current: PlayerData[] = [];
+    const margins = { top: 80, right: 0, bottom: 80, left: 55 };
 
     const X_PADDING = 0.15;
     const SUB_X_PADDING = 0.015;
 
+    let bounds;
+    let svgSize;
+    let graphSize;
+    const colors = [
+        "#74ae59",
+        "#334e69",
+        "#d0af76",
+        "#f64999",
+        "#54d8f8",
+        "#69cccf",
+        "#70ff6b",
+    ];
+
+    const font = "Georgia"
+
     onMount(async () => {
-        d3.csv(datafile).then((data) => {
-            const xScale = d3.scaleBand().padding(X_PADDING)
-            const xSubgroupScale = d3.scaleBand().padding(SUB_X_PADDING);
-            const yScale = d3.scaleLinear();
+        Promise.all([
+            d3
+                .csv("/src/data/PlayerStats/France/Offensive_1.csv")
+                .then((data) => _data.push(transformData(data)))
+                .catch((e) => {
+                    console.error(e);
+                    _data.push([]);
+                }),
+            d3
+                .csv("/src/data/PlayerStats/France/Offensive_2.csv")
+                .then((data) => _data.push(transformData(data)))
+                .catch((e) => {
+                    console.error(e);
+                    _data.push([]);
+                }),
+        ]).then(() => {
+            current = _data[0];
+            setSizing();
+            build();
+        });
 
-            const colNames = Object.values(data.splice(0, 1)[0]).slice(1) as string[];
-            const colKeys = Object.keys(data[0]).slice(1);
-            const parsedData = parseData(colKeys, data).slice(0, 3);
-            const highestColumnKey = getHighestColumnKey(colKeys, parsedData)
+        // Generate graph:
+        const g = d3
+            .select(".graph")
+            .select("svg")
+            .append("g")
+            .attr("id", "graph-g")
+            .attr(
+                "transform",
+                "translate(" + margins.left + "," + margins.top + ")"
+            );
 
+        g.append("g").attr("class", "x axis");
 
-            const colorScale = d3.scaleOrdinal().range(colors).domain(colKeys);
+        g.append("g").attr("class", "y axis");
 
-            // Generate graph:
-            const g = d3.select('.graph')
-                .select('svg')
-                .append('g')
-                .attr('id', 'graph-g')
-                .attr('transform',
-                'translate(' + margins.left + ',' + margins.top + ')')
+        setSizing();
+        build();
 
-            g.append('g')
-                .attr('class', 'x axis')
+        function setSizing() {
+            bounds = (
+                d3.select(".graph").node() as any
+            ).getBoundingClientRect();
 
-            g.append('g')
-                .attr('class', 'y axis')
+            svgSize = {
+                width: bounds.width,
+                height: 600,
+            };
 
-            g.append('text')
-                .text('Comparaison de la performance des joueurs de la France')
-                .style('text-anchor', 'middle')
-                .attr('class', 'title')
+            graphSize = {
+                width: svgSize.width - margins.right - margins.left,
+                height: svgSize.height - margins.bottom - margins.top,
+            };
 
-            setSizing()
-            build()
+            d3.select("#bar-chart")
+                .select("svg")
+                .attr("width", svgSize.width)
+                .attr("height", svgSize.height);
+        }
 
-            function setSizing () {
-                bounds = (d3.select('.graph').node() as any).getBoundingClientRect()
+        function build() {
+            const groups = getDataGroups(current);
+            const subgroups = getDataSubgroups(current);
+            const xScale = d3
+                .scaleBand()
+                .rangeRound([0, graphSize.width])
+                .padding(X_PADDING)
+                .domain([...groups]);
+            const xSubgroupsScale = d3
+                .scaleBand()
+                .padding(SUB_X_PADDING)
+                .domain(subgroups)
+                .rangeRound([0, xScale.bandwidth()]);
+            const yScale = d3
+                .scaleLinear()
+                .rangeRound([graphSize.height, 0])
+                .domain([0, getLargestValue(current)]);
+            const colorScale = d3
+                .scaleOrdinal()
+                .range(colors)
+                .domain(subgroups);
 
-                svgSize = {
-                    width: bounds.width,
-                    height: 600
-                }
+            // Title:
+            d3.select(".title")
+                .attr("x", graphSize.width / 2)
+                .attr("y", -35);
 
-                graphSize = {
-                    width: svgSize.width - margins.right - margins.left,
-                    height: svgSize.height - margins.bottom - margins.top
-                }
+            // X-Axis:
+            d3.select(".x.axis")
+                .attr("transform", `translate(0,${graphSize.height})`)
+                .call((d3.axisBottom(xScale) as any).tickFormat((x) => x));
 
-                d3.select('#bar-chart').select('svg')
-                    .attr('width', svgSize.width)
-                    .attr('height', svgSize.height)
-            }
+            // Y Axis:
+            d3.select(".y.axis").call((d3.axisLeft(yScale) as any).ticks(5));
 
-            function build () {
-                d3.select('.y.axis-text')
-                    .attr('x', -50)
-                    .attr('y', graphSize.height / 2)
-
-                d3.select('.title')
-                    .attr('x', graphSize.width / 2)
-                    .attr('y', -35)
-
-                const xGroups = parsedData.map((d) => d.player);
-                const xSubgroups = colNames;
-                xScale.domain(xGroups).range([0, graphSize.width]);
-                xSubgroupScale.domain(xSubgroups).range([0, xScale.bandwidth()]);
-                yScale.domain([0, findMaximumKeyValue(colKeys, parsedData)]).range([graphSize.height, 0]);
-
-                d3.select('.x.axis')
-                    .attr('transform', `translate(0,${graphSize.height})`)
-                    .call((d3.axisBottom(xScale) as any).tickFormat(x => x.split(' ')[1]))
-                    
-                d3.select('.y.axis').call((d3.axisLeft(yScale) as any).ticks(5))
-                d3.select("#graph-g")
+            // Groups:
+            d3.select("#graph-g")
                 .selectAll(".group")
-                .data(parsedData)
+                .data(current)
                 .join("g")
                 .attr("class", "group")
-                .attr("transform", (group) => `translate(${xScale(group.player as string)})`);
-             
-                d3.select("#graph-g")
+                .attr(
+                    "transform",
+                    (data: PlayerData) => `translate(${xScale(data.player)})`
+                );
+
+            // Subgroups:
+            d3.select("#graph-g")
                 .selectAll(".group")
                 .selectAll("rect")
-                .data((d: any) => d)
-                .join("rect")
-                .attr("width", xSubgroupScale(xSubgroups[1])! - xSubgroupScale(xSubgroups[0])!)
-                .attr("height", (d: any) => graphSize.height - yScale(d[highestColumnKey]))
-                .attr("x", (d: any) => {
-                    console.log(d)
-                    return xSubgroupScale(Object.keys(d)[0])!
-                })
-                .attr("y", (d: any) => yScale(d[Object.keys(d)[0]]))
-                .style("fill", (d: any) => colorScale(Object.keys(d)[0]) as string)
-            }
 
-            window.addEventListener('resize', () => {
-                setSizing()
-                build()
-            })
+                .data((d: any) => [d])
+                .join("rect")
+                .attr("x", (d: PlayerData) => xSubgroupsScale(d.subgroup)!)
+                .attr("y", (d: PlayerData) => yScale(d.value))
+                .attr("width", xSubgroupsScale.bandwidth())
+                .attr(
+                    "height",
+                    (d: PlayerData) => graphSize.height - yScale(d.value)
+                )
+                .attr("class", "hoverable-element")
+                .style(
+                    "fill",
+                    (d: PlayerData) => colorScale(d.subgroup) as string
+                )
+                .on("mouseover", function (_, d: PlayerData) {
+                    d3.select(this).style(
+                        "fill",
+                        d3
+                            .color(colorScale(d.subgroup) as string)!
+                            .darker(0.2)
+                            .formatRgb()
+                    );
+                })
+                .on("mouseout", function (_, d: PlayerData) {
+                    d3.select(this).style(
+                        "fill",
+                        colorScale(d.subgroup) as string
+                    );
+                })
+                .transition()
+                .duration(1000)
+        }
+
+        window.addEventListener("resize", () => {
+            setSizing();
+            build();
         });
     });
 </script>
 
 <div class="graph" id="bar-chart">
-    <svg class="main-svg"></svg>
+    <svg class="main-svg" />
 </div>
+
+<style>
+    * {
+        font-family: Georgia, "Times New Roman", Times, serif !important;
+    }
+
+    :global(.hoverable-element:hover) {
+        cursor: pointer;
+        text-decoration: underline;
+    }
+
+    :global(.tick) {
+        font-size: 8px;
+    }
+</style>
--- src/utils/viz3-helpers.ts
+++ src/utils/viz3-helpers.ts
@@ -1,30 +1,44 @@
-export const parseData = (keys: string[], data: Object[]) => {
-    const returnData: any[] = [];
-    for (const d of data) {
-        const tmp = {};
-        for (const key of keys) {
-            tmp[key] = Number(d[key]);
-        }
+import type { PlayerData } from "src/models/france-player-data";
 
-        returnData.push({...tmp, player: (d as any).player});
+export const transformData = (data: Object[]) => {
+    let n: PlayerData[] = [];
+    for (const d of data) {
+        const tmp: PlayerData[] = [];
+        const keys = Object.keys(d);
+        for (const k of keys) {
+            if (k === "player") {
+                continue;
+            }
+            tmp.push({
+                player: (d as any).player!.split(" ")[1],
+                subgroup: k,
+                value: Number(d[k]),
+            } as PlayerData);
+        }
+        n = n.concat(tmp);
     }
-    return returnData;
-}
+
+    return n.sort((a, b) => b.value - a.value).filter((d) => d.value !== 0);
+};
 
 export const findMaximumKeyValue = (keys: string[], data: Object[]) => {
     let maximum = 0;
     for (const object of data) {
-        const values: number[] = Object.values(object).filter((v) => typeof(v) === "number")
+        const values: number[] = Object.values(object).filter(
+            (v) => typeof v === "number"
+        );
         maximum = Math.max(...values, maximum);
     }
     return maximum;
-}
+};
 
 export const getHighestColumnKey = (keys: string[], data: Object[]) => {
     let highestKey = keys[0];
     let highestKeyValue = 0;
     for (const key of keys) {
-        const keyValues: number[] = data.map((d) => d[key]).filter((d) => typeof(d) === 'number')
+        const keyValues: number[] = data
+            .map((d) => d[key])
+            .filter((d) => typeof d === "number");
         const maximum = Math.max(...keyValues);
         if (highestKeyValue < maximum) {
             highestKey = key;
@@ -32,4 +46,16 @@ export const getHighestColumnKey = (keys: string[], data: Object[]) => {
         }
     }
     return highestKey;
-}
+};
+
+export const getDataGroups = (data: PlayerData[]): string[] => {
+    return [...new Set(data.map((d) => d.player))];
+};
+
+export const getDataSubgroups = (data: PlayerData[]): string[] => {
+    return [...new Set(data.map((d) => d.subgroup))];
+};
+
+export const getLargestValue = (data: PlayerData[]): number => {
+    return Math.max(...data.map((d) => d.value));
+};
